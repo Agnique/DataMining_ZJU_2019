0fac29a2dcb684b60dbc46d255303718b6eda7c5
Agnique <ccaizi@icloud.com>
2019-05-30 19:29:25 +0800

    finished perceptron

--- hw2/linear-models/perceptron.py
+++ hw2/linear-models/perceptron.py
@@ -12,11 +12,25 @@ def perceptron(X, y):
 
     '''
     P, N = X.shape
-    w = np.zeros((P + 1, 1))
+    w = np.ones((P + 1, 1))
     iters = 0
+    D = np.zeros((1,N))
     # YOUR CODE HERE
     
     # begin answer
+    flag = 0
+    w = w.ravel()
+    while iters < 1000 and flag == 0:
+        iters = iters + 1
+        D = np.dot(w[1:].T,X)*y + w[0]*y
+        flag = 1
+        for i in range(N):
+            if D[0][i] <= 0:
+                flag = 0
+                w[1:] = w[1:] + X[:,i]*y[:,i]
+                w[0] = w[0] + y[:,i]
+       
+    
     # end answer
     
     return w, iters
